58d112962fbbba088c07e47aac07fdcadc8c2027
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2023-08-02 14:25:58 +0530

    ceaser cipher.py

--- lab5/Problem1.py
+++ lab5/Problem1.py
@@ -4,6 +4,13 @@ s = input("Enter message: ")
 key = 3
 
 for i in s.lower():
+    
     c = (ord(i)-97 + key)%26
     c = chr(c+97)
+
+    """
+    c = ord(i)+key
+    c = chr(c - (26 if c>122 else 0))
+    """
+    
     print(c,end="")
